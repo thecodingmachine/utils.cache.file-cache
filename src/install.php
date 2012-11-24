6b0f2f896bc211e8532b4bcd6e5ac2193df43495
David Négrier <david@davidtcm>
2012-11-24 12:28:23 +0100

    Fixing installer

---
+++ src/install.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Copyright (c) 2012 David Negrier
+ *
+ * See the file LICENSE.txt for copying permission.
+ */
+
+require_once __DIR__."/../../../autoload.php";
+
+use Mouf\Actions\InstallUtils;
+use Mouf\MoufManager;
+
+// Let's init Mouf
+InstallUtils::init(InstallUtils::$INIT_APP);
+
+// Let's create the instance
+$moufManager = MoufManager::getMoufManager();
+if (!$moufManager->instanceExists("fileCacheService")) {
+	$fileCacheService = $moufManager->createInstance("Mouf\\Utils\\Cache\\FileCache");
+	$fileCacheService->setName("fileCacheService");
+	$fileCacheService->getProperty("defaultTimeToLive")->setValue(3600);
+	if ($moufManager->instanceExists("errorLogLogger")) {
+		$fileCacheService->getProperty("log")->setValue($moufManager->getInstanceDescriptor("errorLogLogger"));
+	}
+}
+
+// Let's rewrite the MoufComponents.php file to save the component
+$moufManager->rewriteMouf();
+
+// Finally, let's continue the install
+InstallUtils::continueInstall();
+?>
